1dbaa1d470f9c3b99cf84f32de4fa955f61f48bb
Martin <mato.hlavna@gmail.com>
2017-01-02 18:26:31 +0100

    Load pre osoby

--- src/main/java/sk/uniza/fri/pds/spotreba/energie/service/SeOsobaService.java
+++ src/main/java/sk/uniza/fri/pds/spotreba/energie/service/SeOsobaService.java
@@ -5,7 +5,13 @@
  */
 package sk.uniza.fri.pds.spotreba.energie.service;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
 import java.util.List;
+import sk.uniza.fri.pds.spotreba.energie.OracleJDBCConnector;
 import sk.uniza.fri.pds.spotreba.energie.domain.SeOsoba;
 
 public class SeOsobaService implements SeService<SeOsoba> {
@@ -23,7 +29,22 @@ public class SeOsobaService implements SeService<SeOsoba> {
 
     @Override
     public List<SeOsoba> findAll() {
-        return null;
+        try (Connection connection = OracleJDBCConnector.getConnection();) {
+            CallableStatement stmnt = connection.prepareCall("SELECT * FROM SE_OSOBA ORDER BY ROD_CISLO ASC");
+            ResultSet result = stmnt.executeQuery();
+            List<SeOsoba> output = new LinkedList<>();
+            while (result.next()) {
+                SeOsoba o = new SeOsoba();
+                o.setRodCislo(result.getString("ROD_CISLO"));
+                o.setIdAdresy(result.getInt("ID_ADRESY"));
+                o.setMeno(result.getString("MENO"));
+                o.setPriezvisko(result.getString("PRIEZVISKO"));
+                output.add(o);
+            }
+            return output;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
