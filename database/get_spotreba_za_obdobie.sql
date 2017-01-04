8ecad0821db5244d4cfe868d5aed75623aa7b8da
Patrik Hrmo <patohrmo@gmail.com>
2017-01-04 18:36:39 +0100

    get spotreba zaciatok

---
+++ database/get_spotreba_za_obdobie.sql
@@ -0,0 +1,11 @@
+create or replace function get_spotreba_za_obdobie(
+    pa_cislo_odberatela SE_ODBERATEL.cislo_odberatela%TYPE,
+    datum_od date,
+    datum_do date,
+    pa_velicina SE_TYP_ZARIADENIA.velicina%TYPE
+)
+return number;
+is 
+begin
+  
+end;
