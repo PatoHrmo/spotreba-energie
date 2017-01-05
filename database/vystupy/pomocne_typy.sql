0022699f14102ce6d6d647f3784ccb757eb100ec
Patrik Hrmo <patohrmo@gmail.com>
2017-01-05 17:43:12 +0100

    3 vystup hotovy

--- database/vystupy/pomocne_typy.sql
+++ database/vystupy/pomocne_typy.sql
@@ -22,3 +22,16 @@ create or replace type zvysena_spotreba_t is object (
 /
 create or replace type zaznamy_zvysenej_spotreby_t is table of zvysena_spotreba_t;
 /
+
+-- pomocky pre 3 vystup
+
+create or replace type info_poruchovost_t is object (
+    cislo_zariadenia integer,
+    pocet_servisov_do_5_rokov integer,
+    pocet_servisov_typu_0 integer,
+    pocet_servisov_typu_1 integer,
+    pocet_servisov_typu_2 integer
+);
+/
+create or replace type zaznamy_info_poruchovost_t is table of info_poruchovost_t;
+/
