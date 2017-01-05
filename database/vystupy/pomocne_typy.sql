2960b59cd4fc5466a78990d560d721c67a06c66e
Patrik Hrmo <patohrmo@gmail.com>
2017-01-05 16:46:47 +0100

    2 vystup implementovany

--- database/vystupy/pomocne_typy.sql
+++ database/vystupy/pomocne_typy.sql
@@ -1,4 +1,6 @@
--- tvorba pomocneho objektu uchovavajuceho obdobie a velkost spotreby v tomto obdobz
+-- pomocky na 1. vystup
+
+-- tvorba pomocneho objektu uchovavajuceho obdobie a velkost spotreby v tomto obdoby
 create or replace type zaznam_o_spotrebe_t is object(
   datum_od date,
   datum_do date,
@@ -7,4 +9,16 @@ create or replace type zaznam_o_spotrebe_t is object(
 /
 -- tvorba pomocnej nested table
 create or replace type zaznamy_o_spotrebe_t is table of zaznam_o_spotrebe_t;
+/
+
+-- pomocky na 2. vystup
+
+create or replace type zvysena_spotreba_t is object (
+  meno Varchar2(40),
+  velicina Varchar2(20),
+  priemerna_spotreba_v_minulosti number,
+  zvysena_spotreba number
+);
+/
+create or replace type zaznamy_zvysenej_spotreby_t is table of zvysena_spotreba_t;
 /
